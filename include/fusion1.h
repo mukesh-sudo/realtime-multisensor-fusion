2d971511cce7541e3ef3012e096cd6c9496a23d7
Mukesh Kumar Durai <mukeshinmail@gmail.com>
2026-04-12 10:45:47 +0800

    Add fusion1 geometric mean computation using log accumulation
    Signed-off-by: Mukesh Kumar Durai <mukeshinmail@gmail.com>

---
+++ include/fusion1.h
@@ -0,0 +1,10 @@
+#ifndef FUSION1_H
+#define FUSION1_H
+
+#include "sliding_window.h"
+
+// Function 1: Geometric mean of all values in the window
+// Returns 0.0 if any value in the window is zero
+double compute_fusion1(const SlidingWindow& window);
+
+#endif // FUSION1_H
